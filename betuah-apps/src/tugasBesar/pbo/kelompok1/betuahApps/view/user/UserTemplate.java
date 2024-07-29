44ce01033b718381359b7d26f029c87fc315c8e5
rratuan <ratua4820@gmail.com>
2024-07-29 13:01:52 +0700

    upload template

---
+++ betuah-apps/src/tugasBesar/pbo/kelompok1/betuahApps/view/user/UserTemplate.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasBesar.pbo.kelompok1.betuahApps.view.user;
+
+/**
+ *
+ * @author ratua
+ */
+public class UserTemplate {
+    
+}
